1a7d8f0168992054e6c108c22360829bb14f6e58
Pontus Hjortskog <p.hjortskog@gmail.com>
2025-09-30 00:36:27 +0200

    Update recent popups to work with spotty data

--- src/components/recent-episodes-popups.tsx
+++ src/components/recent-episodes-popups.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { TvShowDetails } from "@/app/types";
-import { isAfter, parse, subDays } from "date-fns";
+import { isAfter, isBefore, parse, subDays } from "date-fns";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "sonner";
@@ -10,12 +10,23 @@ interface Props {
   favourites: TvShowDetails[];
 }
 
+const isRecent = (dateString: string | undefined): boolean => {
+  if (!dateString) return false;
+  const date = parse(dateString, "yyyy-MM-dd", new Date());
+  const aWeekAgo = subDays(new Date(), 7);
+  return isAfter(date, aWeekAgo) && isBefore(date, new Date());
+};
+
 const useShowsWithRecentEpisodes = (favourites: TvShowDetails[]) => {
   return favourites.filter((show) => {
     const dateStr = show.last_air_date;
-    if (!dateStr) return null;
-    const date = parse(dateStr, "yyyy-MM-dd", new Date());
-    return isAfter(date, subDays(new Date(), 7));
+    const dateStr2 = show.last_episode_to_air?.air_date;
+    // TODO patch this in an API-layer instead
+    const dateStr3 = (
+      show.next_episode_to_air as typeof show.last_episode_to_air
+    )?.air_date;
+
+    return isRecent(dateStr) || isRecent(dateStr2) || isRecent(dateStr3);
   });
 };
 
